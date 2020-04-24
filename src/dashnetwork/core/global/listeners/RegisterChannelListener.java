de694490d9f394b103004befdcd3ff2799d69396
MasterDash5 <dash.flipperx@gmail.com>
2020-04-24 13:32:21 -0600

    actually test for WorldDownloader

--- src/dashnetwork/core/global/listeners/RegisterChannelListener.java
+++ src/dashnetwork/core/global/listeners/RegisterChannelListener.java
@@ -3,6 +3,7 @@ package dashnetwork.core.global.listeners;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 import dashnetwork.core.Core;
+import dashnetwork.core.utils.MessageUtils;
 import dashnetwork.core.utils.User;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -15,12 +16,18 @@ public class RegisterChannelListener implements Listener {
     public void onRegisterChannel(PlayerRegisterChannelEvent event) {
         Player player = event.getPlayer();
         User user = User.getUser(player);
+        String channel = event.getChannel();
 
-        ByteArrayDataOutput output = ByteStreams.newDataOutput();
-        output.writeInt(0);
-        output.writeBoolean(user.isOwner());
+        if (channel.equals("wdl:request")) {
+            ByteArrayDataOutput output = ByteStreams.newDataOutput();
+            output.writeInt(0);
+            output.writeBoolean(user.isOwner());
 
-        player.sendPluginMessage(Core.getInstance(), "wdl:control", output.toByteArray());
+            player.sendPluginMessage(Core.getInstance(), "wdl:control", output.toByteArray());
+        }
+
+        if (user.isDash())
+            MessageUtils.message(user, channel);
     }
 
 }
