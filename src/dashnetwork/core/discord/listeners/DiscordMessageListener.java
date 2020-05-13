2a3dc307c6d0df2915a62704b2bbcd899c001dd8
MasterDash5 <dash.flipperx@gmail.com>
2020-05-13 12:39:29 -0600

    Use clickevent for staffchat discord -> minecraft attachment files

--- src/dashnetwork/core/discord/listeners/DiscordMessageListener.java
+++ src/dashnetwork/core/discord/listeners/DiscordMessageListener.java
@@ -6,12 +6,13 @@ import dashnetwork.core.utils.PermissionType;
 import github.scarsz.discordsrv.DiscordSRV;
 import github.scarsz.discordsrv.api.Subscribe;
 import github.scarsz.discordsrv.api.events.DiscordGuildMessagePreProcessEvent;
-import github.scarsz.discordsrv.dependencies.jda.api.entities.Member;
-import github.scarsz.discordsrv.dependencies.jda.api.entities.Role;
-import github.scarsz.discordsrv.dependencies.jda.api.entities.TextChannel;
-import github.scarsz.discordsrv.dependencies.jda.api.entities.User;
+import github.scarsz.discordsrv.dependencies.jda.api.entities.*;
 import github.scarsz.discordsrv.util.DiscordUtil;
+import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.HoverEvent;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DiscordMessageListener {
 
@@ -19,26 +20,36 @@ public class DiscordMessageListener {
     public void onDiscordChat(DiscordGuildMessagePreProcessEvent event) {
         DiscordSRV discord = DiscordSRV.getPlugin();
         TextChannel channel = event.getChannel();
-        User user = event.getAuthor();
-        Member member = event.getMember();
-        Role role = DiscordUtil.getTopRole(member);
-        String nickname = member.getNickname();
-        String name = nickname == null ? event.getAuthor().getName() : nickname;
-        String tag = user.getAsTag();
-        String display = event.getMessage().getContentDisplay();
 
         if (channel.equals(discord.getDestinationTextChannelForGameChannelName("staffchat"))) {
+            User user = event.getAuthor();
+            Member member = event.getMember();
+            Role role = DiscordUtil.getTopRole(member);
+            Message message = event.getMessage();
+            String nickname = member.getNickname();
+            String name = nickname == null ? event.getAuthor().getName() : nickname;
+            String tag = user.getAsTag();
+            String display = message.getContentDisplay();
+            List<Message.Attachment> attachments = message.getAttachments();
+            boolean hasAttachment = attachments.size() > 0;
             String rank = "&2&lPlayer";
 
             if (role != null)
                 rank = DiscordUtil.convertRoleToMinecraftColor(role) + "&l" + role.getName().split(" ")[0];
 
-            MessageBuilder message = new MessageBuilder();
-            message.append("&9&lStaff ");
-            message.append("&6" + rank + "&6 " + name).hoverEvent(HoverEvent.Action.SHOW_TEXT, "&6" + tag);
-            message.append("&6&l > &6" + display);
+            if (display.isEmpty())
+                display = "&oClick to view attachment";
 
-            MessageUtils.broadcast(true, null, PermissionType.STAFF, message.build());
+            MessageBuilder builder = new MessageBuilder();
+            builder.append("&9&lStaff ");
+            builder.append("&6" + rank + "&6 " + name).hoverEvent(HoverEvent.Action.SHOW_TEXT, "&6" + tag);
+
+            if (hasAttachment)
+                builder.append("&6&l > &6" + display).clickEvent(ClickEvent.Action.OPEN_URL, attachments.get(0).getUrl());
+            else
+                builder.append("&6&l > &6" + display);
+
+            MessageUtils.broadcast(true, null, PermissionType.STAFF, builder.build());
 
             event.setCancelled(true);
         }
